573f844529ce43da64a5e799cc21f981d91917f2
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-01-11 18:13:59 -0700

    Euler Problem 1

--- student_solution.rb
+++ student_solution.rb
@@ -137,6 +137,22 @@ print_item_sums table, table
 
 #################################################################################
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+my_euler_array = (1..999).to_a
+sum = 0
+my_euler_array.each { |num|
+  if num % 3 == 0 || num % 5 == 0
+    sum += num
+  else 
+  end
+}
+# p sum
+# 233168
+
 
 #################################################################################
 
